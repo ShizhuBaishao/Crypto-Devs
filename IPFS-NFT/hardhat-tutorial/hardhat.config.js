be5d077a37b3ac6e8355495093ebdf56d66b0c80
WhiteRainBoy <382898478@qq.com>
2023-04-12 22:02:39 +0800

    feat: ipfs-nft demo

--- IPFS-NFT/hardhat-tutorial/hardhat.config.js
+++ IPFS-NFT/hardhat-tutorial/hardhat.config.js
@@ -7,7 +7,7 @@ const PRIVATE_KEY1 = "a06cd1f81053817f9b38c9ab598c8a80f6066e6360086c1626164da98b
 const ALCHEMY_API_KEY = "YOUER_ALCHEMY_API_KEY";
 const GOERLI_PRIVATE_KEY = "YOUER_GOERLI_PRIVATE_KEY";
 
-//contracts: 0xCD7eE77519843B612d8fE1d89A97776303d2E4cd
+//contracts: 0x18AC1AE9B78f127136EbA4Eddef1be8909a218e8
 module.exports = {
   solidity: "0.8.18",
   defaultNetwork: "goerli",
